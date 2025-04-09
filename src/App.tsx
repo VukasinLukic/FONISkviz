36b3aaeb20581d9e97c0143a494145f69563d9b9
Nikola Kostić <nikolampek04@gmail.com>
2025-04-09 15:15:56 +0200

    sredjeni bugovi

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import { GameProvider } from './context/GameContext';
-import { Route, Routes } from 'react-router-dom';
-import SplashScreen from './pages/SplashScreen';
-import JoinPage from './pages/JoinPage';
-import QRCodePage from './pages/QRCodePage';
-import LobbyPage from './pages/LobbyPage';
-import AdminFlowLayout from './components/AdminFlowLayout';
-import { router } from './Routes';
+import { Outlet } from 'react-router-dom';
 import './index.css';
 
 function App() {
   return (
     <GameProvider>
-      {/* Use RouterProvider directly with the imported router */}
-      <Routes>
-        {/* Main routes from the router */}
-        {router.routes.map((route: any, index: number) => (
-          <Route
-            key={index}
-            path={route.path}
-            element={route.element}
-          />
-        ))}
-      </Routes>
+      <Outlet />
     </GameProvider>
   );
 }
